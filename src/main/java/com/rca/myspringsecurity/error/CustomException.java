bde32242b6882c505b288264272897e0f0e3ec5a
BlessingGianna7 <blessinggianna7@gmail.com>
2025-05-07 08:35:59 +0200

    first commit

--- src/main/java/com/rca/myspringsecurity/error/CustomException.java
+++
@@ -1,8 +0,0 @@
-package com.rca.myspringsecurity.error;
-
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/rca/myspringsecurity/repository/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rca.myspringsecurity.repository;
-
-import com.rca.myspringsecurity.entity.Laptop;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LaptopRepository extends JpaRepository<Laptop, Integer> {
-}
